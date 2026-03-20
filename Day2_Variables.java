7eb994e1619e4bba8ad0f9a69fe1dd3e15d84c4a
Meghana Bhushan SN <bhushanmeghana027@gmail.com>
2026-03-21 00:15:31 +0530

    PHW1:Variables

---
+++ Day2_Variables.java
@@ -0,0 +1,29 @@
+package Daily_Assignments;
+
+public class Day2_Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte age = 21;
+        short marks = 450;
+        byte battery = 85;
+        int cityPopulation = 2500000;
+        char grade = 'A';
+        int bankBalance = 50000;
+        boolean loginStatus = true;
+        short distance = 1200;
+        long largeNumber = 9000000000L;
+        
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Battery: " + battery + "%");
+        System.out.println("City Population: " + cityPopulation);
+        System.out.println("Grade: " + grade);
+        System.out.println("Bank Balance: " + bankBalance);
+        System.out.println("Login Status: " + loginStatus);
+        System.out.println("Distance: " + distance + " meters");
+        System.out.println("Large Number: " + largeNumber);
+
+	}
+
+}
