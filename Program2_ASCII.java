1e363534d8191f7619d329d93759c07878df8fec
Meghana Bhushan SN <bhushanmeghana027@gmail.com>
2026-03-20 08:35:17 +0530

    Trialer

---
+++ Program2_ASCII.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Program2_ASCII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a1 = 'P';
+		char a2 = 'p';
+		System.out.println("ascii value is:"+(a1*a2));
+	}
+
+}
