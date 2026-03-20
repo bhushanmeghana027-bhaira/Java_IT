8ce4c25b9c81189327750b8fe0e3bb55bc7ed3de
Meghana Bhushan SN <bhushanmeghana027@gmail.com>
2026-03-20 11:00:02 +0530

    just cjexk

---
+++ maincheckdummy.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class maincheckdummy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HI ganesha ji ");
+
+	}
+
+}
